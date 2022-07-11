4c973a5f0ad6f36f4463562202d4ef2e82dc72ba
UtkuGokalp <utkugokalp@gmail.com>
2022-07-11 18:12:49 +0300

    Implemented Insert and Clear functions for linked list.

--- include/linkedlist.h
+++ include/linkedlist.h
@@ -8,8 +8,10 @@ typedef struct LinkedList LinkedList;
 LinkedList* CreateLinkedList();
 bool DestroyLinkedList(LinkedList* list);
 
-bool AddToLinkedList(LinkedList* list, const void* data);
+bool AddToLinkedList(LinkedList* list, void* data);
 bool RemoveFromLinkedListAt(LinkedList* list, size_t index);
+bool InsertIntoLinkedList(LinkedList* list, void* data, size_t index);
+bool ClearLinkedList(LinkedList* list);
 
 size_t GetLinkedListLength(const LinkedList* list);
 void* GetLinkedListElementAt(const LinkedList* list, size_t index);
--- src/linkedlist.c
+++ src/linkedlist.c
@@ -2,14 +2,18 @@
 
 typedef struct Node
 {
-    const void* data;
+    void* data;
     struct Node* next;
     struct Node* previous;
 } Node;
 
-Node* CreateNewNode(const void* data, Node* previous, Node* next)
+Node* CreateNewNode(void* data, Node* previous, Node* next)
 {
     Node* new = (Node*)calloc(1, sizeof(Node));
+    if (new == NULL)
+    {
+        return NULL;
+    }
     new->data = data;
     new->previous = previous;
     new->next = next;
@@ -21,6 +25,32 @@ typedef struct LinkedList
     size_t length;
     Node* first_node;
 } LinkedList;
+
+Node* GetNode(const LinkedList* list, size_t index)
+{
+    if (list == NULL)
+    {
+        return NULL;
+    }
+    if (index >= list->length)
+    {
+        return NULL;
+    }
+    if (list->first_node == NULL)
+    {
+        return NULL;
+    }
+
+    Node* current = list->first_node;
+    for (size_t i = 0; i < index; i++)
+    {
+        if (current->next != NULL)
+        {
+            current = current->next;
+        }
+    }
+    return current;
+}
 
 LinkedList* CreateLinkedList()
 {
@@ -55,7 +85,7 @@ bool DestroyLinkedList(LinkedList* list)
     return true;
 }
 
-bool AddToLinkedList(LinkedList* list, const void* data)
+bool AddToLinkedList(LinkedList* list, void* data)
 {
     if (list == NULL)
     {
@@ -68,14 +98,49 @@ bool AddToLinkedList(LinkedList* list, const void* data)
     }
     else
     {
-        Node* current = list->first_node;
-        while (current->next != NULL)
+        Node* current = GetNode(list, list->length - 1);
+        if (current == NULL)
         {
-            current = current->next;
+            return false;
         }
         current->next = CreateNewNode(data, current, NULL);
     }
     list->length++;
+    return true;
+}
+
+bool InsertIntoLinkedList(LinkedList* list, void* data, size_t index)
+{
+    if (list == NULL)
+    {
+        return false;
+    }
+    if (index == 0 && list->first_node == NULL)
+    {
+        return AddToLinkedList(list, data);
+    }
+    if (index >= list->length)
+    {
+        return false;
+    }
+
+    Node* node = GetNode(list, index);
+    if (node == NULL)
+    {
+        return false;
+    }
+    Node* temp = node->previous;
+    node->previous = CreateNewNode(data, temp, node);
+    if (index == 0)
+    {
+        list->first_node = node->previous;
+    }
+    if (temp != NULL)
+    {
+        temp->next = node->previous;
+    }
+    list->length++;
+
     return true;
 }
 
@@ -93,26 +158,52 @@ bool RemoveFromLinkedListAt(LinkedList* list, size_t index)
     {
         return false;
     }
-
-    Node* current = list->first_node;
-    for (size_t i = 0; i < index; i++)
+    if (index == 0) //If removing the first node, list->first_node should be updated.
     {
+        //Previous node is always NULL. Just update the next node.
+        Node* old_first_node = list->first_node;
+        list->first_node = list->first_node->next;
+        if (list->first_node != NULL) //list->first_node will be NULL after the previous line if the list has only one element.
+        {
+            list->first_node->previous = NULL;
+        }
+        free(old_first_node);
+    }
+    else
+    {
+        Node* current = GetNode(list, index);
+        if (current == NULL)
+        {
+            return false;
+        }
+        if (current->previous != NULL)
+        {
+            current->previous->next = current->next;
+        }
         if (current->next != NULL)
         {
-            current = current->next;
+            current->next->previous = current->previous;
+        }
+        free(current);
+    }
+    list->length--;
+    return true;
+}
+
+bool ClearLinkedList(LinkedList* list)
+{
+    if (list == NULL)
+    {
+        return false;
+    }
+    while (list->length > 0)
+    {
+        bool result = RemoveFromLinkedListAt(list, 0);
+        if (result == false)
+        {
+            return false;
         }
     }
-
-    if (current->previous != NULL)
-    {
-        current->previous->next = current->next;
-    }
-    if (current->next != NULL)
-    {
-        current->next->previous = current->previous;
-    }
-
-    list->length--;
     return true;
 }
 
@@ -123,17 +214,15 @@ size_t GetLinkedListLength(const LinkedList* list)
 
 void* GetLinkedListElementAt(const LinkedList* list, size_t index)
 {
+    if (list == NULL)
+    {
+        return NULL;
+    }
     if (index >= list->length)
     {
         return NULL;
     }
 
-    size_t i = 0;
-    Node* current = list->first_node;
-    while (i < index)
-    {
-        current = current->next;
-        i++;
-    }
-    return (void*)current->data;
+    Node* current = GetNode(list, index);
+    return current->data;
 }
