d369d77f62ed10fcf4770e5da1f741dc4f85e3d4
UtkuGokalp <utkugokalp@gmail.com>
2022-07-26 12:04:30 +0300

    Updated fmount to automate mounting even more.

--- fmount/src/main.c
+++ fmount/src/main.c
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#define ArraySize(a) (sizeof(a) / sizeof(a[0]))
+
 const char* get_file_name(const char* file_path)
 {
     int lastSlashIndex = -1;
@@ -22,7 +24,7 @@ const char* get_file_name(const char* file_path)
     return (file_path + lastSlashIndex + 1);
 }
 
-void handle_directory(const char* directory_path)
+void handle_mount_directory(const char* directory_path)
 {
     struct stat st = { 0 };
     if (stat(directory_path, &st) == -1)
@@ -36,6 +38,31 @@ void handle_directory(const char* directory_path)
     }
 }
 
+char* create_string(const char* pieces[], size_t length)
+{
+    size_t string_length = 0;
+    for (size_t i = 0; i < length; i++)
+    {
+        string_length += strlen(pieces[i]);
+    }
+
+    char* string = (char*)calloc(1, string_length + 1);//+1 for '\0'
+    for (size_t i = 0; i < length; i++)
+    {
+        strcat(string, pieces[i]);
+    }
+    return string;
+}
+
+void execute_command(const char* command_pieces[], size_t length)
+{
+    char* command = create_string(command_pieces, length);
+    printf("%s\n", command);
+    system(command);
+    printf("\n");
+    free(command);
+}
+
 int main(int argc, const char* argv[])
 {
     if (argc != 2)
@@ -44,26 +71,40 @@ int main(int argc, const char* argv[])
         return EXIT_FAILURE;
     }
 
-    const char* file_path = argv[1];
-    const char* file_name = get_file_name(file_path);
-    char* directory_path = (char*)calloc(1, strlen("/home/ugklp/Desktop/") + strlen(file_name) + strlen("_Mount") + 1);
-    strcat(directory_path, "/home/ugklp/Desktop/");
-    strcat(directory_path, file_name);
-    strcat(directory_path, "_Mount");
+    const char* mount_file_path = argv[1];
+    const char* mount_file_name = get_file_name(mount_file_path);
+    const char* DESKTOP_PATH = "/home/ugklp/Desktop/";
 
-    //Command: mount file_path directory_path
-    char* command = (char*)calloc(1, strlen("sudo mount ") + strlen(file_path) + strlen(" ") + strlen(directory_path) + 1);
-    strcat(command, "sudo mount ");
-    strcat(command, file_path);
-    strcat(command, " ");
-    strcat(command, directory_path);
+    //Create mount directory
+    const char* directory_path_pieces[] = { DESKTOP_PATH, mount_file_name, "_Mount" };
+    char* mount_directory_path = create_string(directory_path_pieces, ArraySize(directory_path_pieces));
+    handle_mount_directory(mount_directory_path);
 
-    handle_directory(directory_path);
-
+    //Mount
+    const char* mount_command_pieces[] = { "sudo mount ", mount_file_path, " ", mount_directory_path };
     printf("Mounting\n");
-    system(command);
+    execute_command(mount_command_pieces, ArraySize(mount_command_pieces));
 
-    free(command);
-    free(directory_path);
+    //Copy data to a new (non-mounted) directory
+    const char* extract_mount_data_command_pieces[] = { "cp -r ", DESKTOP_PATH, mount_file_name, "_Mount", " ", DESKTOP_PATH, mount_file_name, "_MountData"};
+    printf("Extracting mount data\n");
+    execute_command(extract_mount_data_command_pieces, ArraySize(extract_mount_data_command_pieces));
+
+    //Change permissions for the extracted data directory
+    const char* change_permissions_command_pieces[] = { "chmod +rw ", DESKTOP_PATH, mount_file_name, "_MountData" };
+    printf("Changing permissions\n");
+    execute_command(change_permissions_command_pieces, ArraySize(change_permissions_command_pieces));
+
+    //Unmount the mount directory
+    const char* unmount_command_pieces[] = { "sudo umount ", mount_directory_path };
+    printf("Unmounting mount directory\n");
+    execute_command(unmount_command_pieces, ArraySize(unmount_command_pieces));
+
+    //Remove mount directory
+    const char* remove_mount_dir_command_pieces[] = { "rmdir ", mount_directory_path };
+    printf("Deleting mount directory\n");
+    execute_command(remove_mount_dir_command_pieces, ArraySize(remove_mount_dir_command_pieces));
+
+    free(mount_directory_path);
     return EXIT_SUCCESS;
 }
